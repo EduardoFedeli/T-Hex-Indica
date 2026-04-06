2c0009b23cc0eb6f6783ad0919e909c614b19fd8
Eduardo Fedeli <dudufedeli@outlook.com>
2026-04-06 16:44:43 -0300

    Delete src/app/admin/page.tsx

--- src/app/admin/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function AdminIndexPage() {
-  // Se o cara acessar apenas /admin, joga ele pro Dashboard
-  redirect("/admin/dashboard");
-}
